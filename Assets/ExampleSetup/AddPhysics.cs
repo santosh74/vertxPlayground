0ea223a7d2671af764cbad763c40d2f20a727400
santosh74 <33083682+santosh74@users.noreply.github.com>
2018-06-27 16:56:37 +0100

    fixes for pick and drop objects

---
+++ Assets/ExampleSetup/AddPhysics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPhysics : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void NodeLink_Loaded()
+    {
+        gameObject.AddComponent<Rigidbody>();
+    }
+}
--- Assets/scrips/Player.cs
+++ Assets/scrips/Player.cs
@@ -3,19 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using VertexUnityPlayer;
 
-public class ObjectMovement : MonoBehaviour {
+public class Player : MonoBehaviour {
 
     public float movementSpeed = 10;
+
+    private bool isGrabbed = false;
+
+    private GameObject tempParent = null;
+
+    private GameObject chosenObject;
+
+    private Transform parent;
+
     // Use this for initialization
-    void Start() {
-        Debug.Log("init has been called");
+    IEnumerator Start()
+    {
+
+       yield return new WaitForSeconds(5);
+
+       NodeLink[] vertxObj=  FindObjectsOfType<NodeLink>();
+
+        foreach (NodeLink nodelink in vertxObj)
+        {
+            Debug.Log("Name : " + nodelink.name + "GUID : "  +nodelink.Guid);
+        }
+
+       
     }
 
     // Update is called once per frame
-    void Update() {
- 
-       
+    void Update()
+    {
 
+       // Debug.Log(isGrabbed);
+      
+        if(isGrabbed)
+        {
+            chosenObject.transform.position = transform.position;
+        }
         // check for up arrow key
         if (Input.GetAxis("Vertical") > 0.5)
         {
@@ -27,12 +52,12 @@ public class ObjectMovement : MonoBehaviour {
             // down
             transform.Translate(Vector3.down * Time.deltaTime * movementSpeed);
         }
-        else if (Input.GetAxis("Horizontal") < -0.5)
+        else if (Input.GetAxis("Horizontal") > 0.5)
         {
             // Left arrow
             transform.Translate(Vector3.left * Time.deltaTime * movementSpeed);
         }
-        else if (Input.GetAxis("Horizontal") > 0.5)
+        else if (Input.GetAxis("Horizontal") < -0.5)
         {
             // right
             transform.Translate(Vector3.right * Time.deltaTime * movementSpeed);
@@ -58,38 +83,47 @@ public class ObjectMovement : MonoBehaviour {
         else if (Input.GetButtonDown("A"))
         {
             Debug.Log("A button has been pressed!");
+
             PickupObject();
+        }
+        else if (Input.GetButtonDown("B"))
+        {
+            Debug.Log("B button has been pressed!");
+
+            DropObject();
         }
     }
 
+
     private void OnTriggerEnter(Collider other)
     {
+        Debug.Log("collision : " + other.gameObject.tag);
 
-        Debug.Log("collision detected");
-
-        Debug.Log(other.gameObject.name);
+        chosenObject = other.gameObject.FindNodeLink();
     }
 
-    private void OnTriggerStay(Collider other)
+
+
+    private void OnTriggerExit(Collider other)
     {
-        Debug.Log("OnTriggerStay detected");
+        //chosenObject = null;
     }
-    void OnCollisionEnter(Collision col)
+
+    void PickupObject()
     {
-
-        Debug.Log("collision detected");
-
-        Debug.Log(col.gameObject.name);
        
+        parent = chosenObject.transform.parent;
+        isGrabbed = true;
+        chosenObject.transform.parent = transform;
+
     }
 
-    private void OnCollisionStay(Collision collision)
+    void DropObject()
     {
-        Debug.Log("OnCollisionStay detected");
-    }
-
-    private void PickupObject()
-    {
-        
+        isGrabbed = false;
+        //chosenObject.transform.parent = null;
+        //chosenObject.transform = transform;
+        chosenObject.transform.parent = parent;
+        //chosenObject.transform.
     }
 }
